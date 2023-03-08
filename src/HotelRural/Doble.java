8e4ebb92d6c9a217aa9490b050015cc3b86de7c3
Mario <Marioroblespad@gmail.com>
2023-03-08 14:16:45 +0100

    creadas las clases clientes y reservas, y añadidos comentarios de javadoc

--- src/HotelRural/Doble.java
+++ src/HotelRural/Doble.java
@@ -1,10 +1,31 @@
 package HotelRural;
 
+/**
+ * @author Elena
+ *
+ */
 public class Doble extends Habitaciones{
 
 	boolean minibar;
 	
+	/**
+	 * metodo para que calcule los descuentos
+	 */
 	public void calculoDescuentos() {
 		
+	}
+
+	/**sirve para que te devuelva el valor de minibar
+	 * @return minibar,devuelve el valor de minibar
+	 */
+	public boolean isMinibar() {
+		return minibar;
+	}
+
+	/**setter para establecer el valor de minibar
+	 * @param minibar establece el valor de minibar
+	 */
+	public void setMinibar(boolean minibar) {
+		this.minibar = minibar;
 	}
 }
--- src/HotelRural/Suites.java
+++ src/HotelRural/Suites.java
@@ -1,11 +1,46 @@
 package HotelRural;
 
+/**
+ * @author Elena
+ *
+ */
 public class Suites extends Habitaciones {
 
 	double metros;
 	String servicios;
 	
+	/**metodo para calcular los gastos de la suite
+	 * @param calcular los gatos de la habitacion 
+	 */
 	public void calculoGastos() {
 		
+	}
+
+	/**sirve para que te devuelva el valor de metros
+	 * @return devuelve el valor de metros
+	 */
+	public double getMetros() {
+		return metros;
+	}
+
+	/** setter para establecer el valor de metros
+	 * @param metros establece el valor de los metros
+	 */
+	public void setMetros(double metros) {
+		this.metros = metros;
+	}
+
+	/** sirve para que te devuelva el valor de servicios
+	 * @return servicios, devuelve el valor de servicios
+	 */
+	public String getServicios() {
+		return servicios;
+	}
+
+	/**setter para establecer el valor de servicios
+	 * @param servicios, establece el valor de servicios
+	 */
+	public void setServicios(String servicios) {
+		this.servicios = servicios;
 	}
 }
